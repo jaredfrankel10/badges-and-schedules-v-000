f125d9f4b1891eee0f208b54640c9c247c44d128
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 17:52:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,26 +1,26 @@
 def badge_maker(name)
    return "Hello, my name is #{name}."
  end
- 
- 
+
+
  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
  counter = 1
- 
+
  def batch_badge_creator(attendees)
    message_array = attendees.collect { |name| "Hello, my name is #{name}."
  }
    return message_array
  end
- 
+
  def assign_rooms(attendees)
    room = attendees.collect {|name|"Hello, #{name}! You'll be assigned to room #{attendees.find_index(name) + 1}!"}
    return room
  end
- 
+
  def printer(attendees)
    message_array = batch_badge_creator(attendees)
    room = assign_rooms(attendees)
- 
+
    count = 0
    while count < (attendees.length)
      puts message_array[count]
@@ -28,5 +28,5 @@ def badge_maker(name)
      count += 1
    end
  end
- 
+
  printer(attendees)# Write your code here.
